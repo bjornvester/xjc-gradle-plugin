7eea475bc6611c6c1c3b358f5894abe99177f569
Bjørn Vester <bmv@netcompany.com>
2019-09-06 10:59:28 +0200

    Upgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
 
 tasks.withType<Wrapper> {
     distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "5.6"
+    gradleVersion = "5.6.2"
 }
 
 gradlePlugin {
